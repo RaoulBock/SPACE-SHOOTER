49ee020f3531f041e561ee5e9696068db369c39f
Raoul Bock <anthonyraoulbock01@gmail.com>
2025-07-13 19:43:50 +0200

    first build

---
+++ game.js
@@ -0,0 +1,135 @@
+class SpaceShooter extends Phaser.Scene {
+    constructor() {
+      super('SpaceShooter');
+    }
+  
+    preload() {
+      this.load.image('player', 'assets/player.png');
+      this.load.image('bullet', 'assets/bullet.png');
+      this.load.image('enemy', 'assets/enemy.png');
+      this.load.image('background', 'assets/background.png');
+    }
+  
+    create() {
+      // Add scrolling background
+      this.background = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, 'background')
+        .setOrigin(0, 0);
+  
+      // Player setup
+      this.player = this.physics.add.sprite(this.scale.width / 2, this.scale.height - 80, 'player');
+      this.player.setCollideWorldBounds(true);
+  
+      // Bullet group
+      this.bullets = this.physics.add.group();
+  
+      // Enemy group
+      this.enemies = this.physics.add.group();
+  
+      // Shooting
+      this.cursors = this.input.keyboard.createCursorKeys();
+      this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+  
+      this.lastFired = 0;
+  
+      // Spawn enemies every 1.5 seconds
+      this.enemyTimer = this.time.addEvent({
+        delay: 1500,
+        callback: this.spawnEnemy,
+        callbackScope: this,
+        loop: true
+      });
+  
+      // Score
+      this.score = 0;
+      this.scoreText = this.add.text(20, 20, 'Score: 0', {
+        fontSize: '24px',
+        fill: '#fff',
+        stroke: '#000',
+        strokeThickness: 3
+      });
+  
+      // Collision: bullet hits enemy
+      this.physics.add.overlap(this.bullets, this.enemies, this.hitEnemy, null, this);
+  
+      // Collision: enemy hits player
+      this.physics.add.overlap(this.player, this.enemies, this.playerHit, null, this);
+  
+      this.isGameOver = false;
+    }
+  
+    update(time) {
+      if (this.isGameOver) return;
+  
+      // Scroll background
+      this.background.tilePositionY -= 1;
+  
+      // Movement
+      if (this.cursors.left.isDown) {
+        this.player.setVelocityX(-300);
+      } else if (this.cursors.right.isDown) {
+        this.player.setVelocityX(300);
+      } else {
+        this.player.setVelocityX(0);
+      }
+  
+      // Shooting
+      if (Phaser.Input.Keyboard.JustDown(this.spacebar) && time > this.lastFired + 300) {
+        const bullet = this.bullets.create(this.player.x, this.player.y - 20, 'bullet');
+        bullet.setVelocityY(-400);
+        this.lastFired = time;
+      }
+  
+      // Remove off-screen bullets
+      this.bullets.getChildren().forEach(bullet => {
+        if (bullet.y < -bullet.height) {
+          bullet.destroy();
+        }
+      });
+  
+      // Remove off-screen enemies
+      this.enemies.getChildren().forEach(enemy => {
+        if (enemy.y > this.scale.height + enemy.height) {
+          enemy.destroy();
+        }
+      });
+    }
+  
+    spawnEnemy() {
+      const x = Phaser.Math.Between(50, this.scale.width - 50);
+      const enemy = this.enemies.create(x, -50, 'enemy');
+      enemy.setVelocityY(100);
+    }
+  
+    hitEnemy(bullet, enemy) {
+      bullet.destroy();
+      enemy.destroy();
+  
+      this.score += 10;
+      this.scoreText.setText('Score: ' + this.score);
+    }
+  
+    playerHit(player, enemy) {
+      this.physics.pause();
+      player.setTint(0xff0000);
+      this.isGameOver = true;
+      this.scoreText.setText('Game Over! Score: ' + this.score);
+  
+      this.input.once('pointerdown', () => this.scene.restart(), this);
+      this.input.keyboard.once('keydown-SPACE', () => this.scene.restart(), this);
+    }
+  }
+  
+  const config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    physics: {
+      default: 'arcade',
+    },
+    scene: SpaceShooter,
+    parent: 'game-container',
+    backgroundColor: '#000',
+  };
+  
+  const game = new Phaser.Game(config);
+  
